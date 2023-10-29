6a2e2330ebf66eb4ecd3b836e39fe8def381f25d
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-29 11:04:13 +0100

    add: level class on css

--- js/script.js
+++ js/script.js
@@ -9,7 +9,22 @@ function play() {
   elemGrid.classList.add("grid");
   console.log(elemGrid);
 
-}
+  // seleziono i valori dell'input choice
+  let valInput = document.querySelector("select").value;
+  console.log(valInput);
+
+  // scegliamo il tipo di difficoltà, e poi passiamo al ciclo for
+  let newArray = "";
+
+  if(valInput === "facile"){
+    newArray = gridGame(100);
+  } else if (valInput === "avanzato") {
+      newArray = gridGame(81);
+  } else if (valInput === "difficile") {
+      newArray = gridGame(49)
+  };
+
+};
 
 // Creiamo la funzione che mi dia come risultato un array che va da 1 fino a 100, 81, 49
 function numArray(num) {
