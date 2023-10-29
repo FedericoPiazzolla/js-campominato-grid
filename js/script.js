42d3d0c1adb60c68fcdc8bb0895ebd62539cc54d
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-29 10:49:23 +0100

    add: class grid with js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+const btnPlay = document.getElementById("play");
+btnPlay.addEventListener("click", play);
+console.log(btnPlay);
 
+// funzione al play del button
+function play() {
+  // aggiungo class grid 
+  const elemGrid = document.querySelector(".wrapper > div");
+  elemGrid.classList.add("grid");
+  console.log(elemGrid);
+
+}
 
 // Creiamo la funzione che mi dia come risultato un array che va da 1 fino a 100, 81, 49
 function numArray(num) {
