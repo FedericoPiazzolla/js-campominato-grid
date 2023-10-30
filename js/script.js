ec654f33cd9df9dcb1717a3e486708fdac16e613
FedericoPiazzolla <Federicop257@gmail.com>
2023-10-30 11:24:27 +0100

    add: rnd bombs

--- js/script.js
+++ js/script.js
@@ -11,23 +11,27 @@ function play() {
   let level = document.getElementById("choose-level").value;
 
   // scegliamo il tipo di difficoltà, e poi passiamo al ciclo for
-  let girdSize = "";
+  let gridSize = "";
 
   if(level === 'Easy'){
-    girdSize = numArray(100);
+    gridSize = numArray(100);
   } else if (level === 'Medium') {
-    girdSize = numArray(81);
+    gridSize = numArray(81);
   } else if (level === 'Hard') {
-    girdSize = numArray(49)
+    gridSize = numArray(49)
   };
+
+  // generare le bombe
+  // const bombs = generateBombs(gridSize);
+  // console.log(bombs);
 
   // creo la griglia
   const grid = document.querySelector(".grid");
   grid.innerHTML = "";
 
   // creo il ciclo for per l'array
-  for (let i = 0; i < girdSize.length; i++) {
-    const arrayI = girdSize[i];
+  for (let i = 0; i < gridSize.length; i++) {
+    const arrayI = gridSize[i];
     const div = document.createElement("div");
     div.innerHTML = `<span>${arrayI}</span>`;
     div.classList.add('cell');
@@ -54,6 +58,8 @@ function play() {
   };
 };
 
+/***************************************************************************************/
+
 // Creiamo la funzione che mi dia come risultato un array che va da 1 fino a 100, 81, 49
 function numArray(num) {
   const numbers = [];
@@ -64,4 +70,21 @@ function numArray(num) {
     i++;
   };
   return numbers;
+};
+
+// Funzione che genera un numero random
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// Funzione che genera un array di 16 numeri casuali diversi tra loro e senza ripetizioni compresi tra 1 e max
+function generateBombs(max) {
+  const result = [];
+  while (result.length < 16) {
+    const rndNum = getRndInteger(1, max);
+    if(!result.includes(rndNum)) {
+      result.push(rndNum)
+    };
+  };
+  return result;
 };
